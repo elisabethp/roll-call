357175e22a8d78916b8f1c97913b86d84a9bb459
Elisabeth Petit - Bois <epetitbo@students.kennesaw.edu>
2018-11-27 05:55:35 -0500

    added trainer folder

--- RollCallSystem.py
+++ RollCallSystem.py
@@ -98,9 +98,20 @@ class RollCallSystem:
         # Print the numer of faces trained and end program
         print("\n [INFO] {0} faces trained. Exiting Program".format(len(np.unique(ids))))
 
+
+        self.recognizer.read('trainer/trainer.yml')
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        #iniciate id counter
+        id = 0
+        # names related to ids: example ==> Marcelo: id=1,  etc
+        names = ["None",'Elisabeth'] 
+
         cap = cv2.VideoCapture(0)
         cap.set(3,640) # set Width
         cap.set(4,480) # set Height
+        # Define min window size to be recognized as a face
+        minW = 0.1*cap.get(3)
+        minH = 0.1*cap.get(4)
         while True:
             ret, img = cap.read()
             #img = cv2.flip(img, -1)
@@ -109,13 +120,24 @@ class RollCallSystem:
                 gray,     
                 scaleFactor=1.2,
                 minNeighbors=5,     
-                minSize=(20, 20)
+                minSize = (int(minW), int(minH)),
             )
             for (x,y,w,h) in faces:
                 cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-                roi_gray = gray[y:y+h, x:x+w]
-                roi_color = img[y:y+h, x:x+w]  
-            cv2.imshow('video',img)
+                id, confidence = self.recognizer.predict(gray[y:y+h,x:x+w])
+                
+                # Check if confidence is less them 100 ==> "0" is perfect match 
+                if (confidence < 100):
+                    id = names[id]
+                    confidence = "  {0}%".format(round(100 - confidence))
+                else:
+                    id = "unknown"
+                    confidence = "  {0}%".format(round(100 - confidence))
+                
+                cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
+                cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1) 
+
+            cv2.imshow('camera',img)
             k = cv2.waitKey(30) & 0xff
             if k == 27: # press 'ESC' to quit
                 self.class_obj.printSummary()
